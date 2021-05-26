21cc56a6592e39ffd4c54226d4b64c3dfe390b84
Administrator <git@wondershare.cn>
2021-05-26 15:47:56 +0800

    remove old version

--- mediaio/config.py
+++
@@ -1,8 +0,0 @@
-__version__ = "1.0.0"
-__pypi_username__ = ""
-__pypi_packagename__ = "Online-UniConverter-python"
-__github_username__ = "UniConvert"
-__github_reponame__ = "Online-UniConverter-python"
-__endpoint_map__ = {
-    "live": "https://api.media.io",
-}
